171f852cab96b1c1abd07e08668b3e99ec74de73
ai-Iverson <1129170176@qq.com>
2021-02-18 20:03:34 -0800

    select

--- rpa/应用自动化/数据库.py
+++ rpa/应用自动化/数据库.py
@@ -2,6 +2,8 @@ import pymysql
 import uuid
 from rpa.日志模块.log import output_log
 
+DB_OBJECT = dict()
+
 
 def configuration_database(host, port, user, password, database):
     """
@@ -18,14 +20,43 @@ def configuration_database(host, port, user, password, database):
         db = pymysql.connect(host=host, port=port, user=user, password=password, db=database)
         logger.info(f'mysql:host={host}, port={port}, user={user}, password={password}, db={database}')
         uuid_key = str(uuid.uuid1())
-        db_object = dict()
-        db_object[uuid_key] = db
-        logger.info(f'数据库配置对象：{db_object}')
-        return db_object
+        global DB_OBJECT
+        DB_OBJECT[uuid_key] = db
+        logger.info(f'数据库配置对象：{DB_OBJECT}')
+        return uuid_key
 
     except Exception as e:
+        logger.error(f'错误信息：{e}')
+        raise e
+
+def select_statement(uuid,select,froms,where=None):
+    """
+    select语句
+    :param uuid: 数据库对象
+    :param select: 语句
+    :param froms: 表名
+    :param where: 条件
+    :return:
+    """
+    logger = output_log()
+    global DB_OBJECT
+    db = DB_OBJECT[uuid]
+    cursor = db.cursor()
+    if where:
+        sql = f'select {select} from {froms} where {where};'
+    else:
+        sql = f'select {select} from {froms};'
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        logger.info(f"查询结果：{results}")
+        return results
+    except Exception as e:
+        logger.error(f'错误信息：{e}')
         raise e
 
 
 if __name__ == '__main__':
-    configuration_database("localhost", 3306, "root", "123456", "practice")
+    uuid = configuration_database("localhost", 3306, "root", "123456", "practice")
+    select_statement(uuid,'*','EMPLOYEE')
+
